f7ba6ab0ddb4e9adad5ac4d51d0b2a81568f9b36
mida-hub <rusuden0106@gmail.com>
2021-10-26 16:54:30 +0900

    debug wf

--- ddl/data_lake/make_dataset.sh
+++ ddl/data_lake/make_dataset.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-dataset_id=`pwd | sed "s/.*\///g"`
-bq mk --dataset ${dataset_id}
+file_name=$0
+tmp_dir=`echo ${file_name%/*}`
+dataset_id=`echo ${tmp_dir##*/}`
+
+bq mk --force --dataset ${dataset_id}
