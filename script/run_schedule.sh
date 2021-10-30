7e2a49e37095d500b11d54c95a818cae4a7b08b8
mida-hub <rusuden0106@gmail.com>
2021-10-31 00:17:52 +0900

    schedule

---
+++ script/run_schedule.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# utilファイル読み込み
+. $(dirname $0)/util.sh
+
+dataset_id=$1
+wf_type=$2
+
+query_path="bigquery"
+query_name="table_query.sql"
+
+schedule_daily_json=`yaml_to_json < schedule/daily.yml`
+dataset_tables=$(echo ${schedule_daily_json} | jq .${dataset_id})
+
+for table_id in $(echo ${dataset_tables} | jq -c -r '.[]'); do
+    bash $(dirname $0)/run_query.sh "${query_path}/${dataset_id}/${table_id}/${query_name}" "${wf_type}"
+done
